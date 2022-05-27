472e95938405ad317a8360b696cc8e3ed8eda4fe
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-27 21:38:08 +0300

    add a images model to handle all the images and descriptions/details

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Images(models.Model):
+    """
+    models to handle images
+    """
+    image = models.ImageField(upload_to='images/')
+    date_posted = models.DateTimeField(blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=100)
+    posted_by = models.CharField(max_length=40, default='admin')
+
+
