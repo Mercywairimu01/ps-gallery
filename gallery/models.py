077757f2e38d74f2b4e24d55c5b6e38005ba06ad
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-27 21:41:10 +0300

    defined methods for the image class like update,delete and search by category or location

--- gallery/models.py
+++ gallery/models.py
@@ -13,5 +13,62 @@ class Images(models.Model):
     posted_by = models.CharField(max_length=40, default='admin')
     category = models.ForeignKey('Categories', on_delete=models.CASCADE, default=1)
     location = models.ForeignKey('Locations', on_delete=models.CASCADE, default=1)
+    
+    def __str__(self):
+        return self.title
+    
+    
+    def save_image(self):
+        '''
+        method to save an image
+        '''
+        self.save()
+
+    def delete_image(self):
+        '''
+        method to delete an image
+        '''
+        self.delete()
+    
+    @classmethod
+    def get_all(cls):
+        '''
+        method to get all images
+        '''
+        images = Images.objects.all()
+        return images
+
+    @classmethod
+    def filter_by_location(cls, location):
+        '''
+        method to get images by their locations
+        '''
+        image_location = Images.objects.filter(location__name=location).all()
+        return image_location
+
+    @classmethod
+    def update_image(cls, id, value):
+        '''
+        method to update an image
+        '''
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        '''
+        method to get images by unique id
+        ''' 
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        '''
+        method to search images by category
+        '''
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
+
+
 
 
