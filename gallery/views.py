e664de5ad6a9974510ac47ec385494bc45ee2adf
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-28 14:03:32 +0300

    defined views for various methods and rendered them in their templates

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from .models import Images, Location
 
-from django.http  import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the My Gallery')
+def index(request):
+     images = Images.objects.all()
+     locations = Location.get_all_locations()
+     return render(request,'index.html',{'images':images ,'locations': locations})
+
+def image_location(request, location):
+    images = Images.filter_by_location(location)
+    return render(request, 'location.html', {'location_img': images})
+
+def search(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        searched_images = Images.search_by_category(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'message':message,'images': searched_images})
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'search.html', {'message':message})
