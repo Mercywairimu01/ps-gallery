9b053225d66139da32064d4ac8af26da466e1dd0
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-28 14:05:27 +0300

    updated the models' methods

--- gallery/models.py
+++ gallery/models.py
@@ -7,12 +7,12 @@ class Images(models.Model):
     models to handle images
     """
     image = models.ImageField(upload_to='images/')
-    date_posted = models.DateTimeField(blank=True, null=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
     description = models.TextField(null=True, blank=True)
     title = models.CharField(max_length=100)
     posted_by = models.CharField(max_length=40, default='admin')
-    category = models.ForeignKey('Categories', on_delete=models.CASCADE, default=1)
-    location = models.ForeignKey('Locations', on_delete=models.CASCADE, default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, default=1)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE, default=1)
     
     def __str__(self):
         return self.title
@@ -43,7 +43,7 @@ class Images(models.Model):
         '''
         method to get images by their locations
         '''
-        image_location = Images.objects.filter(location__name=location).all()
+        image_location = Images.objects.filter(location__city=location).all()
         return image_location
 
     @classmethod
@@ -66,7 +66,7 @@ class Images(models.Model):
         '''
         method to search images by category
         '''
-        images = cls.objects.filter(category__name__icontains=category)
+        images = cls.objects.filter(category__name__contains=category)
         return images
 
 class Category(models.Model):
@@ -117,7 +117,7 @@ class Location(models.Model):
         '''
         cls.objects.filter(id=id).update(image=value)
     @classmethod
-    def get_all(cls):
+    def get_all_locations(cls):
         '''
         method to retrive all stored locations
         '''
