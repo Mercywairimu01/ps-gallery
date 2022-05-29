73abc1782865f5c3208268df77ea60f1d22860c3
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-29 18:40:10 +0300

    update readme file

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # ps-gallery
  This is a personal gallery application that allows one to display their photos for others to see, click on a single photo to expand it and  view the details of the photo, view photos based on the location they were taken and one can copy a link to the photo to share with  friends
+
+ ## By Mercy Wairimu
+## User Stories  
+User Can :
+
+* View different photos that interest them  
+* Click a single image to expand it and view the details of that photo  
+* Search for different categories   
+* Copy a link to the photo to share with their friends.  
+* View photos based on the location they were taken.  
+## Behavior Driven Development (BDD)
+The BDD focuses on how a user interacts with the application.
+| Behaviour | Input | Output |
+| :---------------- | :---------------: | ------------------:|
+|Display image details|On load|Images & information about the the images will be displayed ie.location and category|
+|Search for images by category|Search by Category|The images that fit search description will be displayed|
+|Click Image|Click on image| A modal containing image details is shown|
+## Setup and Installation
+
+To get a copy of the project up and running on your local machine for development and testing purposes, **clone** this repository into a **virtual environment** and install the package manager, **pip**.
+See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+Use the package manager pip to install all project requirements. 
+```sh
+(virtual) $ pip install -r requirements.txt
+```
+
+### Installing
+
+To get a development env running, use the **.env.example** file to create a **.env** file with appropriate values
+
+### Running the tests
+
+Run automated tests for this system
+
+```sh
+(virtual) $ python3 manage.py test gallery
+```
+
+### Deployment
+
+With all environment variables changed to suit your local copy of this repository, deploy the application to [Heroku](https://medium.com/@hdsingh13/deploying-django-app-on-heroku-with-postgres-as-backend-b2f3194e8a43) to see it live or simply run it locally
+ ```
+ (virtual) $ python3 manage.py runserver
+ ```
+
+## Technologies Used
+
+* [Django 4.0.4](https://www.djangoproject.com/) - The web framework used
+* [Heroku](https://www.heroku.com/platform) -  Deployment platform
+* [Python 3.8.10](https://www.python.org/) - Backend logic
+* [Postresql](https://www.postgresql.org/) - Database system
+## Known Issues & bugs
+
+No known bug at pesent during development.If you come across a bug reach out to the contacts below.
+[mercy.mambui@student.moringaschool.com](mailto:mercy.mambui@student.moringaschool.com)
+## License
+MIT
+
+Copyright (c) 2022 Mercy Wairimu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+## Authors Info
+Email: mercy.mambui@student.moringaschool.com 
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,145 @@
 from django.test import TestCase
+from .models import Images, Category, Location
 
 # Create your tests here.
+
+class TestImages(TestCase):
+    def setUp(self):
+        '''
+        test method to create Image instances called before all tests
+        '''
+        self.location = Location(city='Nairobi')
+        self.location.save_location()
+        
+        self.category = Category(name='Nature')
+        self.category.save_category()
+        
+        self.img = Images(id=1, title='image', description='nature at its best', location=self.location,category=self.category)
+        
+    def test_instance(self):
+        '''
+        test method to assert instances created during setUp
+        '''
+        self.assertTrue(isinstance(self.img, Images))   
+        
+    def test_save_image(self):
+        '''
+        test method to ensure an Image instance has been correctly saved
+        '''
+        self.img.save_image()
+        img = Images.objects.all()
+        self.assertTrue(len(img) > 0)
+        
+    def test_delete_image(self):
+        '''
+        test method to ensure an Image instance has been correctly deleted
+        '''
+        self.img.delete_image()
+        images = Images.objects.all()
+        self.assertTrue(len(images) == 0)
+        
+    def test_search_image_by_location(self):
+        '''
+        test method to obtain image insatnces by location
+        '''
+        self.img.save_image()
+        found_images = self.img.filter_by_location(location='Nairobi')
+        self.assertTrue(len(found_images) == 1)
+        
+    def test_get_image_by_id(self):
+        '''
+        test method to ensure Image instances can be retrieved by id
+        '''
+        obtained_image = Images.get_image_by_id(self.img.id)
+        
+    def test_search_by_category(self):
+        '''
+        test method to ensure correct searching of an multiple image instances by category
+        '''
+        obtained_image = Images.search_by_category(self.img.category)
+        
+    def test_update_image(self):
+        '''
+        test method to ensure an Image instance has been correctly updated
+        '''
+        self.img.save_image()
+        self.img.update_image(self.img.id, 'images/nature.jpg')
+        updated_img = Images.objects.filter(image='images/nature.jpg')
+        self.assertTrue(len(updated_img) > 0)
+        
+        # location tests
+        
+class TestLocation(TestCase):
+    '''
+    test class for Locations model
+    '''
+    def setUp(self):
+        '''
+        test method to create Location instances called before all tests
+        '''
+        self.location = Location(city='Nairobi')
+        self.location.save_location() 
+        
+    def test_instance(self):
+        '''
+        test method to assert instances created during setUp
+        '''
+        self.assertTrue(isinstance(self.location, Location)) 
+                 
+    def test_delete_location(self):
+        '''
+        test method to ensure a Location instance has been correctly deleted
+        '''
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+        
+    def test_get_all_locations(self):
+        '''
+        test method to ensure all instances of Locations class have been retrieved
+        '''
+        locations = Location.get_all_locations()
+       
+        
+    def test_save_location(self):
+        '''
+        test method to ensure a Location instance has been correctly saved
+        '''
+        self.assertTrue(len(Location.objects.all()) == 1)     
+    
+    
+    # def test_update_location(self):
+    #     '''
+    #     test method to ensure a Location instance has been correctly updated
+    #     ''' 
+    #     update_term = 'kigali'
+    #     self.location.save_location()
+    #     Location.update_location(self.location.id, update_term)  
+    #     updated_one = Location.objects.get(id=self.location.id)
+    #     self.assertEqual(updated_one.location, 'kigali')
+    
+class TestCategory(TestCase):
+    '''
+    test class for Categories model
+    '''
+    def setUp(self):
+        '''
+        test method to create Category instances called before all tests
+        '''
+        self.category = Category(name='Nature')
+        self.category.save_category()
+    def test_instance(self):
+        '''
+        test method to assert Category instances are created during setUp
+        '''
+        self.assertTrue(isinstance(self.category, Category))
+        
+    def test_delete_category(self):
+        '''
+        test method to ensure a Category instance has been correctly deleted
+        '''
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
+        
+   
